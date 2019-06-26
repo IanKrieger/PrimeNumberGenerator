f485454dc06a2d5f833746c87de67296cabddeb3
Ian Krieger <iankrieger@ians-mbp.attlocal.net>
2019-06-25 20:01:47 -0400

    Small naming refactor, initial tests to show implementation functionality

--- src/test/java/impl/PrimeNumberGeneratorTest.java
+++ src/test/java/impl/PrimeNumberGeneratorTest.java
@@ -1,4 +1,35 @@
 package impl;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RunWith(Parameterized.class)
 public class PrimeNumberGeneratorTest {
+
+    private PrimeNumberGenerator primeNumberGenerator;
+
+    public PrimeNumberGeneratorTest(PrimeNumberGenerator primeNumberGenerator) {
+        this.primeNumberGenerator  = primeNumberGenerator;
+    }
+
+    @Test
+    public void generateTest() {
+        Assert.assertNull(primeNumberGenerator.generate(2,3));
+    }
+
+    @Test
+    public void isPrimeTest() {
+        Assert.assertFalse(primeNumberGenerator.isPrime(6));
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> instancesToTest() {
+        return Collections.singletonList(new Object[]{new PrimeNumberGenImpl()});
+    }
+
 }
